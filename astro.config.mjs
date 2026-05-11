3270136a5acfc27c1cfc90c9351fd6858d626375
emilianooferreyra <emilianooferreyra@gmail.com>
2026-05-10 21:59:12 -0300

    chore: update site URL to vercel domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://emilianoferreyra.com',
+  site: 'https://emilianoferreyra.vercel.app',
   vite: {
     plugins: [tailwindcss()]
   },
